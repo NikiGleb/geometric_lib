2eb027892c3174bfff0f775cadb4db3007f4258b
NikiGleb <nikihleb7@gmail.com>
2026-09-09 10:59:12 +0300

    Move files to directory

--- rectangle.py
+++
@@ -1,6 +0,0 @@
-def square(a: float, b: float) -> float:
-    return a * b
-
-def perimeter(a: float, b: float) -> float:
-    return 2 * (a + b)
-
--- triangle.py
+++
@@ -1,6 +0,0 @@
-def perimeter(a: float, b: float, c: float) -> float:
-    return a + b + c
-
-def square(a: float, b: float, c: float) -> float:
-    p = perimeter(a, b, c) / 2
-    return (p * (p - a) * (p - b) * (p - c)) ** 0.5
